694d097f4e141f4a2f8b7afe4513fe84d8afa1fe
NiloyBarua <nbniloyofficial@gmail.com>
2025-02-17 11:41:38 -0800

    anything

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h1 className="text-4xl font-semibold underline">This is Home page</h1>
+            <p>Anything</p>
         </div>
     );
 };

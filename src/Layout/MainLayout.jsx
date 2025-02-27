5bf0cff42a273fdd52a67dba2b688dfa9bc56591
Ridwan4D <ridwanul161@gmail.com>
2025-02-27 23:16:40 +0600

    navbar is created

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,9 +1,13 @@
 import { Outlet } from "react-router";
+import Navbar from "../components/Navbar/Navbar";
 
 const MainLayout = () => {
   return (
     <div>
       {/* navbar */}
+      <div className="container mx-auto">
+        <Navbar />
+      </div>
       {/* outlets */}
       <div>
         <Outlet />
